73420dc66201dc07d922b8424ed68a32231b9494
ran <skyran1278@gmail.com>
2022-07-19 11:03:12 +0800

    feat: add package manager

--- src/app.cc
+++ src/app.cc
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "app.h"
+#include "fmt/format.h"
 #include "lib1.h"
 
 int print_ok() {
@@ -11,7 +12,29 @@ int print_ok() {
 }
 
 int main(int argc, char *argv[]) {
-  std::string input;
+  char charStr[] = "Hello, World!中🐍";
+  wchar_t charWStr[] = L"Hello, World!中🐍";
+
+  std::string str = "Hello, World!中🐍";    // depends on the compiler: utf-8 or
+                                           // big5 (localpages)
+  std::wstring wStr = L"Hello, World!中🐍"; // always utf-16
+
+  // https://stackoverflow.com/questions/29218872/n-format-specifier-program-giving-different-outputs-on-different-compilers-why
+  // int position = 0;
+  // printf("%ls%n\n", charWStr, &position);
+
+  fmt::print("{0}\n", charStr);
+
+  long charStrLen = strlen(charStr);
+  long charWStrLen = wcslen(charWStr);
+
+  long strLen = str.length();
+  long wStrLen = wStr.length();
+
+  std::cout << charStr;
+  // std::cout << charWStr;
+
+  std::string input = "";
 
   std::cout << "Please input sqrt number: ";
   std::cin >> input;
